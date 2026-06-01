0782c4175cf50f6670cf6c7752c680722b29fd1c
MauRoblesss <lucasrobles416@gmail.com>
2026-06-01 21:51:05 +0100

    feat: install versions route and helper addon

--- router/router.go
+++ router/router.go
@@ -75,6 +75,7 @@ func Configure(m *wserver.Manager, client remote.Client) *gin.Engine {
 	{
 		server.GET("", getServer)
 		server.DELETE("", deleteServer)
+		server.GET("/version", getInstalledVersion)
 
 		server.GET("/logs", getServerLogs)
 		server.POST("/power", postServerPower)
